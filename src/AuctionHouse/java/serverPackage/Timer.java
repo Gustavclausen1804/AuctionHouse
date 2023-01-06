e5c7d61f9fa94401e2d7f90e7bbec55a0717a2d3
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2023-01-06 11:18:17 +0100

    sletTest client og opret timer

--- src/AuctionHouse/java/serverPackage/Timer.java
+++ src/AuctionHouse/java/serverPackage/Timer.java
@@ -1,4 +1,15 @@
 package serverPackage;
 
 public class Timer {
+
+    long currentTime;
+
+    public Timer() {
+        this.currentTime = System.currentTimeMillis();
+    }
+
+    public long getTimeStamp() {
+        return currentTime;
+    }
+
 }
